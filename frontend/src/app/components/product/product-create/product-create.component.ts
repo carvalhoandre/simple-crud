0331e951bcbfc9929204936b12321b9bc22ea882
carvalhoandre <andre_carvalho0@live.com>
2024-11-12 17:52:31 -0300

    feat: ProductService

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { MatButtonModule } from "@angular/material/button";
+
+import { ProductService } from "../product.service";
 
 @Component({
-  selector: 'app-product-create',
+  selector: "app-product-create",
   standalone: true,
-  imports: [],
-  templateUrl: './product-create.component.html',
-  styleUrl: './product-create.component.css'
+  imports: [MatButtonModule],
+  templateUrl: "./product-create.component.html",
+  styleUrl: "./product-create.component.css",
 })
 export class ProductCreateComponent {
+  constructor(private productService: ProductService, private router: Router) {}
 
+  navigateToProductCreate(): void {
+    this.productService.showMessage("Produto criado!");
+  }
+
+  cancel(): void {
+    this.router.navigate(["/products"]);
+  }
 }
