27c89b9dc628e39571a1e915d9dcb78b66f15d36
carvalhoandre <andre_carvalho0@live.com>
2024-11-12 11:44:32 -0300

    feat: footer component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { HeaderComponent } from "./components/template/header/header.component";
+import { FooterComponent } from "./components/template/footer/footer.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
   templateUrl: "./app.component.html",
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
 })
 export class AppComponent {}
