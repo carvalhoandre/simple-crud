9a736e5f6a16fa697e3cc5504b5abcd23fea336e
carvalhoandre <andre_carvalho0@live.com>
2024-11-12 15:50:42 -0300

    feat: component home

--- frontend/src/app/components/template/navigation/navigation.component.ts
+++ frontend/src/app/components/template/navigation/navigation.component.ts
@@ -2,10 +2,12 @@ import { Component } from "@angular/core";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
 
+import { HomeComponent } from "../../../views/home/home.component";
+
 @Component({
   selector: "app-navigation",
   standalone: true,
-  imports: [MatSidenavModule, MatListModule],
+  imports: [MatSidenavModule, MatListModule, HomeComponent],
   templateUrl: "./navigation.component.html",
   styleUrl: "./navigation.component.css",
 })
