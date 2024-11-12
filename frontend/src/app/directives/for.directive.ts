20c1f91a11a891e4b90a85ab4fbb0e017d290262
carvalhoandre <andre_carvalho0@live.com>
2024-11-12 16:45:39 -0300

    feat: directive

---
+++ frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,28 @@
+import {
+  Directive,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef,
+} from "@angular/core";
+
+@Directive({
+  selector: "[myFor]",
+  standalone: true,
+})
+export class ForDirective implements OnInit {
+  @Input("myForIn") numbers: number[];
+
+  constructor(
+    private container: ViewContainerRef,
+    private template: TemplateRef<any>
+  ) {
+    this.numbers = [];
+  }
+
+  ngOnInit(): void {
+    for (let number of this.numbers) {
+      this.container.createEmbeddedView(this.template, { $implicit: number });
+    }
+  }
+}
---
+++ frontend/src/app/directives/red.directive.ts
@@ -0,0 +1,11 @@
+import { Directive, ElementRef } from "@angular/core";
+
+@Directive({
+  selector: "[appRed]",
+  standalone: true,
+})
+export class RedDirective {
+  constructor(private el: ElementRef) {
+    el.nativeElement.style.color = "#E35E65";
+  }
+}
