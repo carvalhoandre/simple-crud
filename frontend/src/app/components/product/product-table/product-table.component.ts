f8c00716578d6d091dbcb6b82ed8164b66692cec
carvalhoandre <andre_carvalho0@live.com>
2024-11-13 11:42:39 -0300

    feat: product table

---
+++ frontend/src/app/components/product/product-table/product-table.component.ts
@@ -0,0 +1,33 @@
+import { AfterViewInit, Component, ViewChild } from "@angular/core";
+import { MatTableModule, MatTableDataSource } from "@angular/material/table";
+import { MatPaginatorModule, MatPaginator } from "@angular/material/paginator";
+import { MatSortModule, MatSort } from "@angular/material/sort";
+
+import { ProductService } from "../product.service";
+import { CommonModule } from "@angular/common";
+import { Product } from "../product.model";
+
+@Component({
+  selector: "app-product-table",
+  templateUrl: "./product-table.component.html",
+  styleUrl: "./product-table.component.css",
+  standalone: true,
+  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule],
+})
+export class ProductTableComponent implements AfterViewInit {
+  displayedColumns: string[] = ["id", "name", "price"];
+  dataSource = new MatTableDataSource<Product>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private productService: ProductService) {}
+
+  ngAfterViewInit(): void {
+    this.productService.read().subscribe((products) => {
+      this.dataSource.data = products;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+}
--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -3,10 +3,13 @@ import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 import { MatButtonModule } from "@angular/material/button";
 
+import { MatCardModule } from "@angular/material/card";
+import { ProductTableComponent } from "../../components/product/product-table/product-table.component";
+
 @Component({
   selector: "app-product-crud",
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatCardModule, ProductTableComponent],
   templateUrl: "./product-crud.component.html",
   styleUrl: "./product-crud.component.css",
 })
